d8089ebb9f1e699adf7728143eca15cd17ca08d1
Afrokk <afrasiyabkhan379@gmail.com>
2024-06-04 12:03:44 -0400

    feat(public-web-react): setup antd, add cards

--- Public Web Rework/public-web-react/app/layout.tsx
+++ Public Web Rework/public-web-react/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { AntdRegistry } from "@ant-design/nextjs-registry";
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -14,7 +15,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className="flex flex-col justify-center align-center min-h-screen text-center bg-zinc-100 max-w-screen">
-        {children}
+        <AntdRegistry>{children}</AntdRegistry>
       </body>
     </html>
   );
--- Public Web Rework/public-web-react/app/page.tsx
+++ Public Web Rework/public-web-react/app/page.tsx
@@ -1,13 +1,35 @@
+import { Card, Col, Row, Divider, Flex } from "antd";
+
 export default function Home() {
   return (
     <main>
-      <div className="pt-20">
+      <header className="py-20">
         <h1 className="text-6xl tracking-tighter">
           CHIME/
           <span className="font-bold text-red-500 tracking-normal">FRB</span>
         </h1>
         <h3 className="text-2xl pt-4">Fast Radio Bursts in Realtime</h3>
-      </div>
+      </header>
+      <Divider />
+      <Flex align="center" justify="center">
+        <Row gutter={16}>
+          <Col span={8}>
+            <Card title="Old Name" bordered style={{ width: 300 }}>
+              N/A
+            </Card>
+          </Col>
+          <Col span={8}>
+            <Card title="TNS Name" bordered style={{ width: 300 }}>
+              FRB20181209A
+            </Card>
+          </Col>
+          <Col span={8}>
+            <Card title="Repeater Name" bordered style={{ width: 300 }}>
+              Not a repeater.
+            </Card>
+          </Col>
+        </Row>
+      </Flex>
     </main>
   );
 }
