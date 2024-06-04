1ae03ece4926d1e502bc29a4ceacf53dfc9bb9f4
Afrokk <afrasiyabkhan379@gmail.com>
2024-06-04 14:59:46 -0400

    feat(public-web-react): add footer

---
+++ Public Web Rework/public-web-react/src/components/footer.tsx
@@ -0,0 +1,11 @@
+export default function Footer() {
+  return (
+    <footer className="pb-6 pt-20 px-4">
+      <div className="container mx-auto text-center">
+        <p className="mt-5">
+          © 2024 CHIME/FRB Collaboration. All Rights Reserved.
+        </p>
+      </div>
+    </footer>
+  );
+}
