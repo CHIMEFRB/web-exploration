255ec8fb60efe0db12d6d231bc76167a68b92b80
Afrokk <afrasiyabkhan379@gmail.com>
2024-06-06 21:48:28 -0400

    feat(public-web-react18): add navbar

--- Public Web Rework/public-web-react18/src/App.tsx
+++ Public Web Rework/public-web-react18/src/App.tsx
@@ -1,32 +1,123 @@
-import { BaseStyles, ThemeProvider, Box, Button, Heading } from "@primer/react";
+import { Box, Header, Avatar, Heading, Text } from "@primer/react";
 
 function App() {
   return (
-    <ThemeProvider>
-      <BaseStyles>
-        <Box m={4}>
-          <Heading as="h2" sx={{ mb: 2 }}>
-            CHIME/FRB
-          </Heading>
-          <p>Fast Radio Bursts in Realtime.</p>
-          <Button
+    <>
+      <Header
+        bg="primary"
+        sx={{ display: "flex", flexDirection: "row", justifyContent: "center" }}
+      >
+        <Box sx={{ width: "1200px" }}>
+          <Box
             sx={{
-              padding: "20px",
-              backgroundColor: "red",
-              color: "white",
-              borderRadius: "5px",
-              border: "none",
-              cursor: "pointer",
-              ":hover": {
-                color: "black",
-              },
+              display: "flex",
+              flexDirection: "row",
+              justifyContent: "center",
+              maxWidth: "1200px",
+              margin: "0 auto",
             }}
           >
-            Boop!
-          </Button>
+            <Header.Item>
+              <Header.Link
+                sx={{
+                  "&:hover": {
+                    color: "#fff35",
+                  },
+                }}
+                href="#"
+              >
+                <Heading
+                  as="h2"
+                  sx={{
+                    fontWeight: "light",
+                    span: {
+                      fontWeight: "bold",
+                      letterSpacing: "0.1rem",
+                    },
+                  }}
+                >
+                  CHIME/
+                  <span>FRB</span>
+                </Heading>
+              </Header.Link>
+            </Header.Item>
+            <Header.Item
+              full
+              sx={{
+                display: "flex",
+                flexDirection: "row",
+                justifyContent: "end",
+                gap: 3,
+              }}
+            >
+              <Header.Item>
+                <Header.Link href="#">About</Header.Link>
+              </Header.Item>
+              <Header.Item>
+                <Header.Link href="#">Releases</Header.Link>
+              </Header.Item>
+              <Header.Item>
+                <Header.Link href="#">Team</Header.Link>
+              </Header.Item>
+            </Header.Item>
+            <Header.Item
+              sx={{
+                mr: 0,
+              }}
+            >
+              <Avatar
+                src="https://github.com/octocat.png"
+                size={32}
+                square
+                alt="@octocat"
+              />
+            </Header.Item>
+          </Box>
         </Box>
-      </BaseStyles>
-    </ThemeProvider>
+      </Header>
+      <Box m={4} sx={{ maxWidth: "1200px", margin: "0 auto" }}>
+        <Heading as="h2" sx={{ m: 4, textAlign: "center" }}>
+          Event Viewer
+        </Heading>
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "center",
+            gap: 3,
+          }}
+        >
+          <Box
+            sx={{
+              p: 3,
+              borderWidth: 1,
+              borderStyle: "solid",
+              borderColor: "border.default",
+            }}
+          >
+            1
+          </Box>
+          <Box
+            sx={{
+              p: 3,
+              borderWidth: 1,
+              borderStyle: "solid",
+              borderColor: "border.default",
+            }}
+          >
+            2
+          </Box>
+          <Box
+            sx={{
+              p: 3,
+              borderWidth: 1,
+              borderStyle: "solid",
+              borderColor: "border.default",
+            }}
+          ></Box>
+        </Box>
+      </Box>
+    </>
   );
 }
 
--- Public Web Rework/public-web-react18/src/main.tsx
+++ Public Web Rework/public-web-react18/src/main.tsx
@@ -1,9 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { ThemeProvider, BaseStyles } from "@primer/react";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider>
+      <BaseStyles>
+        <App />
+      </BaseStyles>
+    </ThemeProvider>
   </React.StrictMode>
 );
