9cab5051be75ab0e9901f79be2e926e2246e9cea
Afrokk <afrasiyabkhan379@gmail.com>
2024-06-12 11:27:50 -0400

    feat(pipelines-visualization): setup project

---
+++ Pipelines Visualization/pipelines-viz-vue/app.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="flex flex-col justify-center align-center min-h-screen text-center bg-zinc-100 max-w-screen overflow-hidden">
+    <NuxtPage/>
+  </div>
+</template>
---
+++ Pipelines Visualization/pipelines-viz-vue/pages/index.vue
@@ -0,0 +1,6 @@
+<template>
+    <div>
+        <h1 class="text-4xl font-bold tracking-wide">Pipelines Visualization</h1>
+        <p class="pt-2">Nuxt 3 + PrimeVue + Tailwind</p>
+    </div>
+</template>
